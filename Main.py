37d6f2ccf77fa7ba0521691fd0409bebe711f88b
Matthew Mills <Matthew.Mills.21@cnu.edu>
2026-04-20 11:45:43 -0400

    init

---
+++ Main.py
@@ -0,0 +1,5 @@
+# do for every place/airport ticker pair in a list
+# go to wunderground + get high for the airport https://www.wunderground.com/hourly/{airport ticker}/date/{year}-{month}-{day}
+# go to robinhood website and get cheapest contract that is < 3 degrees of high
+# https://robinhood.com/us/en/prediction-markets/climate/events/{place}-daily-temperature-high-{full month name}-{day}-{year}-{month abreviated name}-{day}-{year}/
+# return temperature of cheapest contract found and what place it is in
